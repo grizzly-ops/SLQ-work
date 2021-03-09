6302059beeb260731f1d8a8f1f4f55cbdffcc93f
ndabe <stonerlife420k@gmail.com>
2021-03-09 08:52:56 +0200

    SLQ work

---
+++ analysis/SQL work.sql
@@ -0,0 +1,12 @@
+CREATE TABLE char_data_types(
+varchar_column varchar(10),
+char_column char(10),
+text_column text
+);
+
+INSERT INTO char_data_types
+VALUES ('abc','abc','abc'),
+('defghi','defghi','defghi');
+
+COPY char_data_types TO 'C:\typetest\typetest.txt'
+WITH (FORMAT CSV, HEADER, DELIMITER '|');
---
+++ try it your self/zoo.sql
@@ -0,0 +1,5 @@
+CREATE TABLE animals(
+animle_name varchar(25),
+animle_age varchar(25),
+animle_weight varchar(50)
+);
